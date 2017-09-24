d849246eeb17956caacf212d14e93c148c3baf27
sophiehelf <sophie.helf@gmail.com>
2017-09-24 15:17:08 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
+function getIt() {
+	$('p').on('click', function() {
+		alert("hey!");
+	})
+}
+
+function frameIt() {
+	$('img').on('load', function(){
+		$('img').addClass('tasty');
+	})
+}
+
+function pressIt() {
+	$('input').on('keydown', function(e) {
+		if (e.which === 71) {
+			alert("You pressed g")
+		}
+	})
+}
+
+function submitIt() {
+	$('form').on('submit', function() {
+		alert('Your form is going to be submitted now.');
+	})
+}
 
 $(document).ready(function(){
 
-// call functions here
+	getIt();
+	frameIt();
+	submitIt();
+	pressIt();
 
 });
